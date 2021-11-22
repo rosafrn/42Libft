463b13124d854698146e07f00a917b9ea1d7440b
Rosa Fernandes <rosferna@student.42lisboa.com>
2021-11-22 18:13:17 +0000

    modify putnbr and strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rosferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 15:55:36 by rosferna          #+#    #+#             */
-/*   Updated: 2021/11/19 20:27:15 by rosferna         ###   ########.fr       */
+/*   Updated: 2021/11/22 18:02:34 by rosferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@ char	*ft_strrchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 		i++;
-	while (--i >= 0 || s[i] == c)
-		if (s[i] == c)
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
+		i--;
+	}
 	return (NULL);
 }
 
 /*int main(void)
 {
-	char *c = "rosa fernandes okoko";
-	printf("strrchr: %s\n", strrchr(c, 'r'));
-	printf("ft_strrchr: %s\n", ft_strrchr(c, 'r'));
+	char *c = "xosa fernandes okoko";
+	printf("strrchr: %s\n", strrchr(c, 'x'));
+	printf("ft_strrchr: %s\n", ft_strrchr(c, 'x'));
 }*/
